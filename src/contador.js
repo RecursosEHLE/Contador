18d949e39a6d250b8b995ec277ecf5128da455a6
RecursosEHLE <67298914+RecursosEHLE@users.noreply.github.com>
2020-10-05 19:36:43 -0500

    Update contador.js

--- src/contador.js
+++ src/contador.js
@@ -3,16 +3,29 @@ export class Button extends React.Component {
         super(props);
         this.state = {count: 0};
       }
-      
+           
       sum = () => {
-        this.setState({count: this.state.count +1});
+       this.interval = setInterval(() => {
+       this.setState({count: this.state.count +1});
+        }, 1000)
+      }
+
+      stop = () =>{
+        clearInterval(this.interval);
+      }
+
+      reset = () => {
+        this.setState({count: 0});
       }
 
     render() {
       return (
-        React.createElement('div', {className: 'alert alert-primary'}, 
-        React.createElement('button', {onClick: this.sum, className: 'btn btn-primary'}, 'Contar'),
-        React.createElement('h1', {className: 'display-2'}, this.state.count)
+        React.createElement('div', {className: 'alert alert-primary'},
+        React.createElement('div', {className: 'btn-group'}, 
+        React.createElement('button', {onClick: this.sum, className: 'btn btn-primary'}, 'Start'),
+        React.createElement('button', {onClick: this.stop, className: 'btn btn-danger'}, 'Stop')),
+        React.createElement('h1', {className: 'display-2'}, this.state.count),
+        React.createElement('button', {onClick: this.reset, className: 'btn btn-warning'}, 'Reset')
       ));
     }
   }
